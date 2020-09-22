13fec477def0d54ed937fc41f61d9634c2020387
rogerho <rogerho@umich.edu>
2020-09-21 21:06:58 -0400

    submission flag added

--- hw_fetcher.py
+++ hw_fetcher.py
@@ -8,11 +8,13 @@ class Course:
         self.name = name
 
 class Task:
-    def __init__(self, id, course_id, name, due_date):
+    def __init__(self, id, course_id, name, due_date, others_submitted, submitted):
         self.id = id
         self.course_id = course_id
         self.name = name
         self.due_date = due_date
+        self.others_submitted = others_submitted
+        self.submitted = submitted
 
 CANVAS_TOKEN = "1770~KBb4GdfNpBhVafrsMBlCVnTAfBZQGx4iCZ0QFqv28GPiPnzcFgMW66v8vrLy9rYY"
 USER_ID = "501847"
@@ -47,13 +49,14 @@ todo = list()
 
 for i in courses:
     ass_url = f"https://umich.instructure.com/api/v1/courses/{i.id}/assignments"
-    ass_r = requests.get(ass_url, headers=header, params=payload)
+    ass_payload = {'per_page':100, 'include':['submission']}
+    ass_r = requests.get(ass_url, headers=header, params=ass_payload)
     ass_parsed = json.loads(ass_r.text)
 
     for j in ass_r.json():
         due_date = datetime.strptime(j['due_at'], "%Y-%m-%dT%H:%M:%SZ")
         if due_date > datetime.now():
-            todo.append(Task(j['id'], j['course_id'], j['name'], j['due_at']))
+            todo.append(Task(j['id'], j['course_id'], j['name'], j['due_at'], j['has_submitted_submissions'], 'submitted_at' in j))
 
 for u in todo:
     print(u.__dict__)
